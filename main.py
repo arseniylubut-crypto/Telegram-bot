fdb2ff2d30e49bb7bc1338c885b33559dcd5fbab
arseniylubut-crypto <arseniy.lubut@gmail.com>
2026-07-11 18:22:18 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import logging
+import os
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token=os.getenv("BOT_TOKEN"))
+dp = Dispatcher()
+@dp.message(CommandStart())
+async def cmd_start(message: types.Message):
+ await message.answer("Привет! Я готоч к работе")
+import asyncio
+
+async def main():
+ await dp.start_polling(bot)
+
+if __name__ == "__main__":
+ asyncio.run(main())
